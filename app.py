6306f4e05b6b1d2921db034639c2b14fdbcb48f1
Andy Huang <andy2244759@gmail.com>
2022-03-20 17:47:27 +0800

    Sticker

--- app.py
+++ app.py
@@ -52,7 +52,10 @@ def handle_message(event):
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        StickerSendMessage(
+            package_id='1',
+            sticker_id='1'
+        ))
 
 
 if __name__ == "__main__":
