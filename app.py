b445ad49c3797469c7982d46afd183eed0a00e2d
Andy Huang <andy2244759@gmail.com>
2022-03-20 18:05:15 +0800

    Sticher

--- app.py
+++ app.py
@@ -42,8 +42,8 @@ def handle_message(event):
 
     if "給我貼圖" in msg:
         sticker_message = StickerSendMessage(
-            package_id='1',
-            sticker_id='1'
+            package_id='2',
+            sticker_id='23'
         )
 
         line_bot_api.reply_message(
